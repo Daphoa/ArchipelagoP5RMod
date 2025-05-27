a6fcaeb94793ab9e0cea455f26e8b0d9cc0b1b4f
Daphoa <daphoa@gmail.com>
2025-05-27 18:15:41 -0400

    Added a simple check to ensure we don't show more than one message at a time, even in race conditions.

--- ArchipelagoP5RMod/GameCommunicators/MessageManipulator.cs
+++ ArchipelagoP5RMod/GameCommunicators/MessageManipulator.cs
@@ -6,6 +6,10 @@ public class MessageManipulator
 {
     private readonly FlagManipulator _flagManipulator;
 
+    private static readonly object MessageDisplayingLock = new();
+
+    private static bool showingMessage = false;
+
     private IHook<FlowFunctionWrapper.FlowFuncDelegate8> _msgWndDps;
     private IHook<FlowFunctionWrapper.FlowFuncDelegate8> _msgWndCls;
 
@@ -25,6 +29,27 @@ public class MessageManipulator
 
     private ulong MsgWndDpsImpl()
     {
+        // Only one message can be shown at once. 
+        bool acquiredLock = false;
+        try
+        {
+            Monitor.Enter(MessageDisplayingLock, ref acquiredLock);
+
+            if (showingMessage)
+            {
+                MyLogger.DebugLog("Can't show message, it is already showing.");
+                return 0;
+            }
+            showingMessage = true;
+        }
+        finally
+        {
+            if (acquiredLock)
+            {
+                Monitor.Exit(MessageDisplayingLock);
+            }
+        }
+
         _flagManipulator.SetBit(FlagManipulator.SHOWING_GAME_MSG, true);
 
         return _msgWndDps.OriginalFunction();
@@ -32,8 +57,11 @@ public class MessageManipulator
 
     private ulong MsgWndClsImpl()
     {
+        var retVal = _msgWndCls.OriginalFunction();
+
+        showingMessage = false;
         _flagManipulator.SetBit(FlagManipulator.SHOWING_GAME_MSG, false);
 
-        return _msgWndCls.OriginalFunction();
+        return retVal;
     }
 }
